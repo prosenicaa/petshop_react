47334410815e360439afa4b8e485c79d4cf57eae
Andjela Prosenica <andjelaprosenica@gmail.com>
2023-02-01 23:44:41 +0100

    Napravljena Home stranica

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Background from "../img/background3.jpg";
+import "../styles/Home.css";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="home" style={{ backgroundImage: `url(${Background})` }}>
+      <div className="headerContainer">
+        <h1> The Pet Zone </h1>
+        <p> WHERE YOUR PETS WANT TO GO!</p>
+        <Link to="/shop">
+          <button> ORDER NOW </button>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
